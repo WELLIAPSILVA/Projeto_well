8b7cf1fea1826420fa879a0c12d98c69aca708ea
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-06-29 18:47:40 -0300

    desafio arastar e soltar

--- clicarArrastarSoltar.py
+++ clicarArrastarSoltar.py
@@ -1,11 +1,11 @@
 #como pegar e arrastar algo pra outro lugar
 import pyautogui
-for i in range(9):#função pra repetir o codigo abaixo 9x
+for i in range(6):#função pra repetir o codigo abaixo 6x
     #mover mouse ate ma coordenada
-    pyautogui.moveTo(1423,478,duration=0.5)
+    pyautogui.moveTo(1288,197,duration=0.5)
     # clicar e arrastar ate um ponto e soltar
-    pyautogui.dragTo(1360,790,button='left',duration=0.5)
-    #repetir 9x para cada arquivo (ou ajustar as vezez para quantos arquivos tiver)
+    pyautogui.dragTo(1323,691,button='left',duration=0.5)
+    #repetir 6x para cada arquivo (ou ajustar as vezez para quantos arquivos tiver)
     #para repetir bastar transforma o codigo em função
 
 #Exemplo de aplicação:
