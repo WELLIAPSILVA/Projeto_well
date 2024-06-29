d091dd17e9f5d1b977e6fae55af073aa0c6b3f67
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-06-29 02:07:09 -0300

    explicações cliques

--- especialistaClick.py
+++ especialistaClick.py
@@ -1,4 +1,18 @@
 #como usar a funçao click
 import pyautogui
-
+#click personalizado
 pyautogui.click(x=1000,y=500,clicks=2,interval=0.0,button='left',duration=2)
+#click posição atual (com botão esquerdo)
+pyautogui.click()#clica onde esta o mouse parado
+pyautogui.click(button='left') #clica onde esta o mouse parado com botao esquerdo
+pyautogui.click(button='right')#clica onde esta o mouse parado com botao direito
+pyautogui.click(button='midle')#clica onde esta o mouse parado com botao do meio
+
+#clicar X vezes
+pyautogui.click(clicks=10) #clcia 10x
+# funções para cliques
+pyautogui.doubleClick()#clica onde esta o mouse parado duas vezes
+pyautogui.rightClick() #clica onde esta o mouse parado com botao esquerdo
+pyautogui.middleClick()#clica onde esta o mouse parado com botao direito
+pyautogui.tripleClick()#clica onde esta o mouse parado tres vezes
+
