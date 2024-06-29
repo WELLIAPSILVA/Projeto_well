d3c0ffdd4d90fdf20714ffba4b54d19ab3cc5138
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-06-29 17:51:39 -0300

    criando nova pasta na pasta

---
+++ desafioClic.py
@@ -0,0 +1,11 @@
+import pyautogui 
+from time import sleep
+pyautogui.click(1526,667, button='right')
+sleep(2)
+pyautogui.moveTo(1596,575)
+sleep(2)
+pyautogui.click()
+sleep(2)
+pyautogui.moveTo(1457,578)
+sleep(2)
+pyautogui.click()
