2fb62f0e4b64ee98d6a6f052d012228ed1a129c4
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-06-29 22:59:43 -0300

    digitar senha do e-mail

---
+++ botoesEAtalhosTeclado.py
@@ -0,0 +1,19 @@
+#como usar botões e atalhos do teclado
+import pyautogui
+from time import sleep
+#navegar e clicar no campo e mail
+pyautogui.click(1411,265,duration=2)
+sleep(1)
+#igittar o o e- mail
+pyautogui.typewrite('welliapsilva@gmail.com') 
+sleep(1)
+#apertar tab para o proximo campo
+pyautogui.press('tab')
+sleep(1)
+#digitar senha
+pyautogui.typewrite('112345') #caso a senha tenha caracteres especiais usar função
+#apertar tab
+pyautogui.press('tab')
+sleep(1)
+#apertar espaço
+pyautogui.press('space')
