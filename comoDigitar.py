a6f41028bc3a0a1c89407b7afcf0fdcc32f2db99
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-06-29 22:46:39 -0300

    digitar v2 usar caracteres epeciais

--- comoDigitar.py
+++ comoDigitar.py
@@ -1,12 +1,19 @@
 #como digitar
 import pyautogui
+import pyperclip #permite digitar com carecteres epeciais, porém te que usar funçao
+
+
+def escrever_frase(frase): #function
+    pyperclip.copy(frase) #opia a frase da função
+    pyautogui.hotkey('ctrl','v') #copiar
 #mover o mouse ate o camp de digitar
 pyautogui.moveTo(1585,1009,duration=2)
 #clicar no campo de digitar
 pyautogui.click()
 
-#digitar testeo
-pyautogui.typewrite('Olá, bom dia')#obs, este não aceita careteres epeciais
+#digitar testo
+escrever_frase('Olá, bom dia') #frase que sera usada na função acima
+#pyautogui.typewrite('Olá, bom dia')#obs, este não aceita careteres epeciais
 
 #clicar em enviar
 pyautogui.click(1880,1003,duration=2)
