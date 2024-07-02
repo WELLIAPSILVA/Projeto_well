1562923b78959d753ea24b5a8a180b4cf85e2f54
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-01 23:36:15 -0300

    v1

--- pedirDadosEusarEles.py
+++ pedirDadosEusarEles.py
@@ -1,4 +1,16 @@
 import pyautogui
+#gera um alerta ao usuario
+pyautogui.alert(text='Iniciando aua automação',title='Automaçao de login',button='ok')
 
+#pedir informação ao usuario
 email = pyautogui.prompt(text='Digite seu e-mail:',title='informações Obrigatórias')
 print(f'voce digitou: {email}')
+
+#confirmar  se algo deve acontecer
+resposta= pyautogui.confirm(text='continuar rodando a nossa automação?', title='confirmação', buttons=['sim','não','cancelar'])
+if resposta == 'sim':
+        print('continuar automação')
+elif resposta == 'não':
+        print('Encerrando automação')
+else:
+        print('Operação cancelada')
