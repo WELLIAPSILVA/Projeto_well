bbc99a3c3f00559963316d105ffc014fe74f51de
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-03 00:29:32 -0300

    desafio captcha

--- desafioCaptcha.py
+++ desafioCaptcha.py
@@ -1,11 +1,11 @@
 #recoonhecimento de imagem simples
 import pyautogui
-
-#este laocaliza a imgem que  foi printada e colcaoda na pasta #usa o print pra imprimir os dados na tela
-print(pyautogui.locateOnScreen('cap.png'))
+cnpj= pyautogui.prompt(text='digite o cnpj ',title='CNPJ')
 
 #encontrar a coordenada o centro da imagem
-print(pyautogui.locateCenterOnScreen('cap.png'))
-
+captcha= pyautogui.locateCenterOnScreen('cap.png')
+pyautogui.click(1192,597,duration=2)
+#digitando o site
+pyautogui.typewrite(cnpj)
 #clicando no centro do captcha do site da fazenda.gov
 pyautogui.click(x=1588, y=568,duration=2)
