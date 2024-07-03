6a251be7c4cfe9081231e3665a6ff1cdcf16d63e
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-03 00:39:35 -0300

    explciacaçõ de todas as aberturas

--- navegarAteSites.py
+++ navegarAteSites.py
@@ -1,2 +1,9 @@
 import webbrowser
-webbrowser.open('www.facebook.com')
+#abrir um site em uma aba de navegardor existe ou criar outro(caso esteja logado ele ja abrirá logado)
+webbrowser.open('www.facebook.com') 
+
+#abrir uma nova aba em um navegador existe ou criar outro
+webbrowser.open_new_tab('www.facebook.com')
+
+#abrir um novo navegador
+webbrowser.open_new('www.facebook.com')
