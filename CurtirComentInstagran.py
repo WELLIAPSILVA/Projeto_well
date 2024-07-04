0203e11891d2d18352bb20d464f5212e92edc668
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-03 21:11:40 -0300

    requisitos

---
+++ CurtirComentInstagran.py
@@ -0,0 +1,19 @@
+'''
+bot de curdidas e comentario instagram
+
+1- navegar ate a o site do instragram
+2- entrar com seu usuario
+3- entrar com a senha
+4- clicar em login
+5- clicar em 'not now' para não salvar no navegador
+6- fechar janela de "salvar senha"
+7- pesquisar pela pagina
+8- entar na pagina
+9- clicar na primiera postagem mais recente
+10- verificar se já curti ou não aquela postagem
+11- se ja tiver curtido fazer nada, pasar o bot por 24 horas
+12- se não tiver curtido, curtir a foto
+13- se não tiver curtido comentar a foto
+14- pausar por 24 horas
+15- após 24 hora rodar tudo de novo
+'''
