04e9ebaa27f3a18eb9bbbb25e3180167a945d949
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-03 22:29:04 -0300

    bot curtir e comentar instagram

--- CurtirComentInstagran.py
+++ CurtirComentInstagran.py
@@ -16,4 +16,37 @@ bot de curdidas e comentario instagram
 13- se não tiver curtido comentar a foto
 14- pausar por 24 horas
 15- após 24 hora rodar tudo de novo
-'''
+'''   
+import pyautogui as pa 
+import webbrowser as wb 
+from time import sleep
+login= pa.prompt(text='Digite seu login',title='login')#solicita o usuario
+senha= pa.password(text='Digite sua senha',title='senha',mask='*')#solicita a senha
+pagina= pa.prompt(text='Digite o nome da pagina a buscar',title='Pagina do insta')#solicita a pagina a buscar
+
+wb.open_new('https://www.instagram.com/')
+sleep(4)
+usu=pa.locateCenterOnScreen('instUser.png')
+pa.moveTo(usu[0],usu[1])
+pa.doubleClick()
+sleep(1)
+pa.typewrite(login)
+pa.move(-90,40,duration=1)
+pa.doubleClick()
+pa.typewrite(senha)
+pa.move(-30,50,duration=1)#clica no botao de login
+pa.click(duration=1)
+sleep(5)
+save=pa.locateCenterOnScreen('agoraNao.png')#clicar em 'not now' para não salvar no navegador
+pa.click(save[0],save[1],duration=0.5)
+sleep(5)
+lupa=pa.locateCenterOnScreen('buscar.png')#clicar em buscar para digitar a pagina
+pa.click(lupa[0],lupa[1],duration=0.5)
+sleep(2)
+pa.typewrite(pagina)#digitando a pagina
+page= pa.locateCenterOnScreen('nike.png')#clicar em 'not now' para não salvar no navegador
+pa.click(page[0],page[1],duration=0.5)
+sleep(3)
+public=page= pa.locateCenterOnScreen('publicacoes.png')#navega a ate a escria publicações proximo a ultima publicação
+pa.moveTo(public[0],public[1],duration=0.5)
+sleep(5)
