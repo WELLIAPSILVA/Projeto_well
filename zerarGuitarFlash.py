ca7a504afeb457d6a7e59e59a0766de1a0cfeb50
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-07 16:30:29 -0300

    comentado

--- zerarGuitarFlash.py
+++ zerarGuitarFlash.py
@@ -14,3 +14,8 @@ while keyboard.is_pressed('1') == False:
         sleep(0.05)
 
         #Link para o jogo: https://guitarflash.com/?lg=pt
+
+        '''pixelMatchesColor, isso verificfar se uma determinada cor existe e dai ele clica na poscição ond ela esta
+        pyautogui.pixelMatchesColor(1291,722,(12,152,33)), os dois primeiro numeros sao as coordenadas do click co mouse, e os outros ters são as cooredenada da cor
+        para pegar a cor, quando usar o mouse info, basta apertar f5 com o mouse sobre a cor desejada
+        '''
